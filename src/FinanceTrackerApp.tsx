ab777033b3c6a645e8b3d9e68d2148ae158f03aa
Pedro Perez <pedrodrums2015@gmail.com>
2026-06-13 15:27:25 -0400

    create app

---
+++ src/FinanceTrackerApp.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export const FinanceTrackerApp = () => {
+  return (
+    <>
+      <h1>Finance Tracker App</h1>
+      <Button>Hola mundo</Button>
+    </>
+  );
+};
